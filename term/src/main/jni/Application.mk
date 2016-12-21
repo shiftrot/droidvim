08d9e1ef085840fa5070ceed8a68dcbc65466b43
shiftrot <shiftrot@gmail.com>
2016-12-21 09:32:41 +0900

    Add NDK proper settings

--- term/src/main/jni/Application.mk
+++ term/src/main/jni/Application.mk
@@ -1,2 +1,20 @@
+APP_PLATFORM := android-16
 APP_ABI := armeabi x86 mips
 
+APP_CFLAGS := -Werror=return-type -Werror=implicit-function-declaration -Wno-multichar
+APP_CFLAGS += -DHAVE_PTHREADS
+APP_CFLAGS += -Wno-format
+#APP_CPPFLAGS := -std=c++11
+#APP_CPPFLAGS += -D__STDC_LIMIT_MACROS -D__STDC_CONSTANT_MACROS
+
+APP_THIN_ARCHIVE := true
+
+ifneq (,$(findstring clang,$(NDK_TOOLCHAIN_VERSION)))
+APP_CFLAGS += -Wno-return-type-c-linkage
+endif
+
+# [ShrinkFileSize] It's safe to say this because JNI_OnLoad and SWIG generated functions are marked as exported.
+APP_CFLAGS += -fvisibility=hidden
+
+# [ShrinkFileSize] Tell the linker to garbage collect unused sections.
+APP_LDFLAGS += -Wl,--gc-sections
