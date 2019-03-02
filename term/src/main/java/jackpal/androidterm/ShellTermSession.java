9abef2819f36693da9ab246b8a634148abc8a403
shiftrot <shiftrot@gmail.com>
2019-03-02 13:31:00 +0900

    Add COLORFGBG

--- term/src/main/java/jackpal/androidterm/ShellTermSession.java
+++ term/src/main/java/jackpal/androidterm/ShellTermSession.java
@@ -98,15 +98,16 @@ public class ShellTermSession extends GenericTermSession {
             path = checkPath(path);
         }
 
-        String[] env = new String[8];
-        env[0] = "TERM=" + settings.getTermType();
-        env[1] = "PATH=" + path;
-        env[2] = "HOME=" + TermService.getHOME();
+        String[] env = new String[9];
+        env[0] = "PATH=" + path;
+        env[1] = "HOME=" + TermService.getHOME();
+        env[2] = "TMPDIR=" + TermService.getTMPDIR();
         env[3] = "APPBASE=" + TermService.getAPPBASE();
         env[4] = "APPFILES=" + TermService.getAPPFILES();
         env[5] = "APPEXTFILES=" + TermService.getAPPEXTFILES();
         env[6] = "INTERNAL_STORAGE=" + TermService.getEXTSTORAGE();
-        env[7] = "TMPDIR=" + TermService.getTMPDIR();
+        env[7] = "TERM=" + settings.getTermType();
+        env[8] = "COLORFGBG=" + settings.getCOLORFGBG();
 
         String[] envCmd = env;
         if (mFirst) {
