0962bc66dd79ab993fed6427ba0bbeb93421e77e
shiftrot <shiftrot@gmail.com>
2022-05-30 12:56:36 +0900

    Improve internal storage link

--- term/src/main/java/jackpal/androidterm/TermService.java
+++ term/src/main/java/jackpal/androidterm/TermService.java
@@ -135,7 +135,14 @@ public class TermService extends Service implements TermSession.FinishCallback {
             if (modeHome != 0) mHOME = mAPPEXTHOME;
             boolean appExtHome = prefs.getBoolean("scoped_storage_startup_path_is_APPEXTHOME", false);
             if (appExtHome) mSTARTUP_DIR = mAPPEXTHOME;
-            mEXTSTORAGE = mAPPEXTHOME;
+            File internalDir = Environment.getExternalStorageDirectory();
+            if (!internalDir.canWrite()) {
+                mEXTSTORAGE = "/Android/data/"+BuildConfig.APPLICATION_ID+"/files";
+                internalDir = new File(mEXTSTORAGE);
+                if (!internalDir.canWrite()) {
+                    mEXTSTORAGE = mAPPEXTFILES;
+                }
+            }
         } else {
             mEXTSTORAGE = Environment.getExternalStorageDirectory().toString();
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
